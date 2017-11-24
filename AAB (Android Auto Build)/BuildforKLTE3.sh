3799430a46269dd570c2442bb17e05dfa11d0f2a
KingDiamondDev <33332932+KingDiamondDev@users.noreply.github.com>
2017-11-23 21:34:16 -0500

    Delete BuildforKLTE3.sh

--- AAB (Android Auto Build)/BuildforKLTE3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Now we have to configure Jack so it doesn't run out of memory and creates errors."
-echo "Configuring..."
-export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4G"
-echo "Done! Now let's proceed."
-echo "Time to start building! Are you ready? Let's gooo!"
-croot
-brunch klte
