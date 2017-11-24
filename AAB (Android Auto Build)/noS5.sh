85ff0fa693ca6400c3b5536421a03505519753c2
KingDiamondDev <33332932+KingDiamondDev@users.noreply.github.com>
2017-11-23 21:35:06 -0500

    For Samsung S5 (klte)

---
+++ AAB (Android Auto Build)/noS5.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "All right then. You don't have an S5? All right. Good thing there is a work around. So, I am first going to download the zip file needed for this operation."
+
+echo "You need to click (or copy and paste) this link and download the file to continue."
+
+echo "https://drive.google.com/file/d/1sd95gJPn2hjbWtVYOFWo80VVUGKsyGvC/view?usp=sharing"
+
+echo "Once done, start nos52.sh to continue."
